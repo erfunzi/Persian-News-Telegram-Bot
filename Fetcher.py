b21f30daf33cbfdcad276ace60e87e8717fe334a
Programmer2011bird <maysamshaker7@gmail.com>
2025-09-05 00:48:28 +0330

    Implemented a way to swtich between rss sources/feed + fixed a typo

--- Fetcher.py
+++ Fetcher.py
@@ -5,12 +5,30 @@ import os
 
 dotenv.load_dotenv()
 
-CNN_RSS_FEED: str | None = os.getenv("cnn_rss_feeed")
-BBC_RSS_FEED: str | None = os.getenv("bbc_rss_feeed")
+CNN_RSS_FEED: str | None = os.getenv("cnn_rss_feed")
+BBC_RSS_FEED: str | None = os.getenv("bbc_rss_feed")
 ALJAZEERA_RSS_FEED: str | None = os.getenv("aljazeera_rss_feed")
 REUTERS_RSS_FEED: str | None = os.getenv("reuters_rss_feed")
-# TODO: SWITCH SOURCES IF THE CURRENT ONE IS DOWN / HASN'T BEEN UPDATED
-FEED = feedparser.parse(CNN_RSS_FEED)
+
+FEED_PRIORITY_QUEUE: list[str | None] = [
+    CNN_RSS_FEED,
+    BBC_RSS_FEED,
+    ALJAZEERA_RSS_FEED,
+    REUTERS_RSS_FEED
+]
+
+# TODO: SWITCH SOURCES IF THE CURRENT ONE IS DOWN ( the "bozo" field would flip to 1 )/ HASN'T BEEN UPDATED
+# TODO: FIX SOME ISSUES WITH GETTING THE INFO FROM OTHER SOURCES ( more flexibility probably )
+
+for feed in FEED_PRIORITY_QUEUE:
+    out = feedparser.parse(feed)
+    
+    if out["bozo"] == False:
+        FEED = out
+        break
+
+    if out["bozo"]:
+        pass
 
 def get_news_rss() -> tuple[str, list]:
     source: str = str(FEED.feed.title)
@@ -36,4 +54,4 @@ def get_news_rss() -> tuple[str, list]:
 
 
 if __name__ == "__main__":
-    print(get_news_rss())
+    print(get_news_rss()[0])
