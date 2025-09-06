59bf8a96bf8fd16d92848f94b8db6fd92da715c7
Programmer2011bird <maysamshaker7@gmail.com>
2025-09-07 01:30:05 +0330

    Added Some Todos and Placeholders for future code

--- code/Database.py
+++ code/Database.py
@@ -9,6 +9,19 @@ def to_hash(content: str) -> str:
 
     return hash
 
+class DATABASE: 
+    def __init__(self) -> None:
+        # TODO
+        pass
+    
+    def add_to_db(self, hash: str) -> None:
+        # TODO
+        pass
+
+    def check_db(self, hash: str) -> None:
+        # TODO
+        pass
+
 
 if __name__ == "__main__":
     print(to_hash("South Sudan says Mexico provides assurances its national would not face torture or other inhumane treatment."))
