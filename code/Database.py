42ac7d51ad1804642f38cd187a6a8f4cdf2a08d8
Programmer2011bird <maysamshaker7@gmail.com>
2025-09-08 01:36:41 +0330

    Connecting to the database and creating a table

--- code/Database.py
+++ code/Database.py
@@ -1,4 +1,16 @@
+import psycopg
 import hashlib
+import dotenv
+import os
+
+
+dotenv.load_dotenv()
+
+DATABASE_NAME: str | None = os.getenv("database")
+USER: str | None = os.getenv("user")
+PASSWORD: str | None = os.getenv("password")
+HOST: str | None = os.getenv("host")
+PORT: str | None = os.getenv("port")
 
 
 def to_hash(content: str) -> str:
@@ -11,8 +23,18 @@ def to_hash(content: str) -> str:
 
 class DATABASE: 
     def __init__(self) -> None:
-        # TODO
-        pass
+        self.CONN = psycopg.connect(
+            dbname=DATABASE_NAME,
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT
+        )
+
+        self.CURSOR = self.CONN.cursor()
+
+        self.CURSOR.execute("CREATE TABLE IF NOT EXISTS hashes (id SERIAL PRIMARY KEY, hash text)")
+        print("TABLE CREATED SUCCESSFULLY")
     
     def add_to_db(self, hash: str) -> None:
         # TODO
@@ -25,3 +47,4 @@ class DATABASE:
 
 if __name__ == "__main__":
     print(to_hash("South Sudan says Mexico provides assurances its national would not face torture or other inhumane treatment."))
+    db = DATABASE()
